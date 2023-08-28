aeed15008a6b11746cb9c0b61cf7972a922c3f54
Mike B. K <mikekristiansen88@gmail.com>
2023-08-28 22:20:33 +0200

    Update 28/8
    Gallery section and page added and done (+responsive)
    
    Navbar hide on scroll
    
    footer added

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,10 @@
+// Hide nav bar on scroll down and show on scroll up
+
 const mainScroll = document.querySelector('.main_container'); //Get the main container
 
 let lastScrollTop = 0; //This Varibale will store the top position
 
 let navbar = document.querySelector('#navbar_hide'); //Get the navbar
-
-/*
-mainScroll.addEventListener('scroll', function () {
-  //on every scroll this funtion will be called'
-  let scrollTop = mainScroll.scrollTop;
-  //This line will get the location on scroll
-
-  console.log(scrollTop);
-  if (scrollTop > lastScrollTop) {
-    //if it will be greater than the previous
-    navbar.style.top = '-80px';
-    //set the value to the negetive of height of navbar
-  } else {
-    navbar.style.top = '0';
-  }
-  lastScrollTop = scrollTop; //New Position Stored
-});
-*/
 
 let hideTopAt = 500;
 
@@ -31,11 +15,18 @@ mainScroll.addEventListener('scroll', function () {
 
   if (scrollTop > hideTopAt) {
     //if it will be greater than the previous
-    navbar.style.top = '-80px';
+    navbar.style.top = '-60px';
     //set the value to the negetive of height of navbar
   } else {
     navbar.style.top = '0';
   }
 });
 
-Fancybox.bind(); //! Activere fancybox
+// Footer Open Script
+const openFooterMenu = document.querySelector('#footerBtn');
+openFooterMenu.addEventListener('click', expandFooterMenu);
+
+function expandFooterMenu() {
+    document.querySelector('#footerBtn').classList.toggle('btnFooter_open');
+    document.querySelector('.footer').classList.toggle('footerOpen');
+}
