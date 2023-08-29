20a664de5c6645df18774a9e6a70b3e42bda425c
Mike B. K <mikekristiansen88@gmail.com>
2023-08-29 13:55:36 +0200

    Sounds player
    Sounds player added

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+
 // Hide nav bar on scroll down and show on scroll up
 
 const mainScroll = document.querySelector('.main_container'); //Get the main container
@@ -27,6 +28,68 @@ const openFooterMenu = document.querySelector('#footerBtn');
 openFooterMenu.addEventListener('click', expandFooterMenu);
 
 function expandFooterMenu() {
-    document.querySelector('#footerBtn').classList.toggle('btnFooter_open');
-    document.querySelector('.footer').classList.toggle('footerOpen');
+  document.querySelector('#footerBtn').classList.toggle('btnFooter_open');
+  document.querySelector('.footer').classList.toggle('footerOpen');
 }
+
+//player
+
+duration = document.querySelector('#duration');
+current = document.querySelector('#current');
+playPause = document.querySelector('#playPause');
+
+let timeCalculator = function (value) {
+  second = Math.floor(value % 60);
+  minute = Math.floor((value + 60) / 60);
+
+  if (second < 10) {
+    second = '0' + second;
+  }
+
+  return minute + ':' + second;
+};
+
+// start wavesurfer object
+
+wavesurfer = WaveSurfer.create({
+  container: '#wave',
+  waveColor: 'darkgrey',
+  progressColor: '#1aafff',
+  height: 48,
+  srollParent: false,
+});
+
+//load audio file
+wavesurfer.load('./sound/space-hole-test-23665.mp3');
+
+//play and pause a player
+playPause.addEventListener('click', function (e) {
+  wavesurfer.playPause();
+});
+
+//load audio duration on load
+wavesurfer.on('ready', function (e) {
+  duration.textContent = timeCalculator(wavesurfer.getDuration());
+});
+
+//get updated current time on play
+wavesurfer.on('audioprocess', function (e) {
+  current.textContent = timeCalculator(wavesurfer.getCurrentTime());
+});
+
+//change play button to pause button on play
+wavesurfer.on('play', function (e) {
+  playPause.classList.remove('fa-play');
+  playPause.classList.add('fa-pause');
+});
+
+//change pause button to play button on pause
+wavesurfer.on('pause', function (e) {
+  playPause.classList.add('fa-play');
+  playPause.classList.remove('fa-pause');
+});
+
+//update time on seek
+wavesurfer.on('seek', function (e) {
+  current.textContent = timeCalculator(wavesurfer.getCurrentTime());
+});
